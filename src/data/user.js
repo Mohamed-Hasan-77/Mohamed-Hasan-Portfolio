42e83d91e2c1a54be359fe39ee9ccc65b8d5efd8
Mohamed-Hasan-77 <0mdx3210@gmail.com>
2023-10-20 03:13:06 +0200

    GG

---
+++ src/data/user.js
@@ -0,0 +1,118 @@
+const INFO = {
+	main: {
+		title: "Mohamed Hasan",
+		name: "Mohamed Hasan",
+		email: "mahmedhasnn@gmail.com",
+		// logo: "../logo.png",
+		logo: "../MohamedIcon.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/MoHasan92198119",
+		github: "https://github.com/Mohamed-Hasan-77",
+		linkedin: "https://www.linkedin.com/in/mohamed-hasan-962a5a204/",
+		instagram: "https://www.instagram.com/mo7amed7asan7/",
+		// stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/profile.php?id=100013081533163",
+	},
+
+	homepage: {
+		title: "I,m Mohamed Hasan, a Front-End developer ",
+		description:
+			"Self-motivated Front-End developer with experience in building interactive and responsive web applications. Proficient in HTML, CSS, JavaScript, and modern frameworks like React and Redux Toolkit. Passionate about creating visually stunning and user-friendly interfaces.",
+	},
+
+	about: {
+		title: "Hello, I’m Mohamed Hasan. ",
+		description:
+			`Enthusiastic and dedicated front-end React developer with a passion for creating engaging and intuitive user interfaces. Armed with a solid foundation in HTML, CSS, and JavaScript, I am well-versed in modern front-end technologies including Bootstrap, Tailwind CSS, TypeScript, Git, Redux, and SCSS. My proficiency is not just theoretical, I've successfully applied my skills in diverse projects, showcasing my ability to deliver high-quality, responsive web applications.
+			What sets me apart is not only my technical proficiency but also my relentless commitment to learning. A fast learner by nature, I have a hunger for knowledge that drives me to stay abreast of the latest industry trends and advancements.`,
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "E-Commerce FreshCart",
+			description:
+				"e-commerce app using React featuring real API integration, secure payment gateway, order tracking, and a fully functional login and register system.",
+			logo: "https://img.icons8.com/cute-clipart/64/react-native.png",
+			linkText: "View Project",
+			link: "https://e-commerce-fresh-cart.vercel.app/",
+		},
+
+		{
+			title: "Trending Movies App",
+			description:
+				" trending movies React app using React, tailwind, powered by TheMovieDB API, offering a feature-rich experience including movie showcases, actor details, search functionality, and dynamic dark and light modes",
+			logo: "https://img.icons8.com/cute-clipart/64/react-native.png",
+			linkText: "View Project",
+			link: "https://trending-movies-app-nine.vercel.app/",
+		},
+
+		{
+			title: "Admin Dashboard",
+			description:
+				"admin dashboard using React, typeScript and scss , showcasing insightful charts powered by Recharts, and providing at-a-glance access to user and product lists.",
+			logo: "https://img.icons8.com/cute-clipart/64/react-native.png",
+			linkText: "View Project",
+			link: "https://react-admin-dashboard-kohl.vercel.app/",
+		},
+
+		{
+			title: "Marvel Portfolio ",
+			description:
+				"simple portfolio website with a Modern design .sleek dark mode and very nice animations ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://marvel-portfolio-chi.vercel.app/",
+		},
+
+		{
+			title: "Photo Editor",
+			description:
+				"an online Photo editor in which you can upload and download image and apply your filters ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://photo-editor-umber.vercel.app/",
+		},
+		{
+			title: "DANIELS",
+			description:
+				"simple website with a Modern design and very nice animations HTML-CSS-Bootstrap",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://daniels-company.vercel.app/#",
+		},
+		{
+			title: "DevFolio",
+			description:
+				"simple website with a Modern design and very nice animations using HTML-CSS-Bootstrap ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://dev-portfolio-one-rho.vercel.app/",
+		},
+		{
+			title: "Construction services",
+			description:
+				" website for Construction services with a responsive and nice design using vanilla HTML-CSS",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://construction-services-company-e8u2fyk7r-mohamed-hasan-77.vercel.app/",
+		},
+		{
+			title: "Kasper Company website template",
+			description:
+				" website for digital company with a responsive and nice design using vanilla HTML-CSS",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://kasber-company-portfolio.vercel.app/",
+		},
+	],
+};
+
+export default INFO;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import Socials from "../components/about/socials";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/contact.css";
+
+const Contact = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "contact");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Contact | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="contact" />
+				<div className="content-wrapper">
+					<div className="contact-logo-container">
+						<div className="contact-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+
+					<div className="contact-container">
+						<div className="title contact-title">
+							Let's Get in Touch: Ways to Connect with Me
+						</div>
+
+						<div className="subtitle contact-subtitle">
+							Thank you for your interest in getting in touch with
+							me. I welcome your feedback, questions, and
+							suggestions. If you have a specific question or
+							comment, please feel free to email me directly at
+							&nbsp;{" "}
+							<a href={`mailto:${INFO.main.email}`}>
+								Email Me 
+							</a>
+							
+						</div>
+					</div>
+
+					<div className="socials-container">
+						<div className="contact-socials">
+							<Socials />
+						</div>
+					</div>
+
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Contact;
